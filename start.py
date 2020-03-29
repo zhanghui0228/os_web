abaaf3284cac18bf038087785f068356491100d4
hui <helloworldhui@163.com>
2020-03-29 15:42:52 +0800

    添加时间戳

--- start.py
+++ start.py
@@ -1,10 +1,13 @@
 import os
+from datetime import datetime
 from logzero import logfile, logger
 from web_system import settings
 
 
 log_path = os.path.join(settings.BASE_DIR, "log") 
 run_log = os.path.join(log_path, "run.log")
+# now time
+now_time = datetime.now()
 try:
     if os.path.exists(log_path) == False:
         os.mkdir(log_path)
@@ -15,7 +18,7 @@ logfile(run_log, maxBytes=3000000, backupCount=2, encoding="utf-8")
 
 Pid = "ps -elf|grep 'manage.py'|grep -v grep|awk '{print $4}'"
 # 启动命令
-start = "nohup python3 {0}/manage.py runserver 0.0.0.0:9000 >log/jenkins_work.log&".format(settings.BASE_DIR)
+start = "echo {0} >log/jenkins_work.log && nohup python3 {1}/manage.py runserver 0.0.0.0:9000 >>log/jenkins_work.log&".format(now_time, settings.BASE_DIR)
 lines_pid = os.popen(Pid)
 
 # kill process id
