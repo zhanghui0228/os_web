ce1792936a88ae409fbaaeb3299317040033e2fc
hui <helloworldhui@163.com>
2020-04-01 14:56:16 +0800

    通知消息添加时间戳

--- web_system/check_site.py
+++ web_system/check_site.py
@@ -1,6 +1,7 @@
 '''
     检查脚本，供计划任务使用
 '''
+from datetime import datetime
 from logzero import logfile, logger, logging
 from . import check_healthy
 from . import check_option
@@ -14,11 +15,12 @@ logfile("log/cron.log", maxBytes=3000000, backupCount=2, encoding="utf-8")
 # url_healthy = check_healthy.local_healthy()
 # 系统基础信息
 basics_healthy = check_option.useagent()
+now = datetime.now()
 
 
 
 def healthy():
-    local_info = [basics_healthy]
+    local_info = ["巡检信息，时间：{}".format(now), basics_healthy]
     try:
         # 钉钉机器人通知
         message.message_notice(local_info)
